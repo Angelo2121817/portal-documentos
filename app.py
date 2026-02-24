206478cd4fdbc009733a2adb6a1f7ced6562c40d
Angelo2121817 <metalquimicaconsultoria@gmail.com>
2026-02-24 16:15:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ if not os.path.exists(COFRE_DIR):
 
 # --- FUNÇÃO DE ENVIO DE E-MAIL (STREAMLIT + GMAIL) ---
 def enviar_email_com_anexo(nome_documento, conteudo_arquivo, nome_arquivo_original):
-    try:
-        # No Streamlit, usamos st.secrets!
-        email_remetente = st.secrets[documentosmetalquimica@gmail.com
-]
-        senha_remetente = st.secrets[ppvl tuta vkoa ucil]
-        email_destino = st.secrets[metalquimicaconsultoria@gmail.com]
+        try:
+        # No Streamlit, usamos as "etiquetas" (nomes das variáveis)
+        email_remetente = st.secrets["EMAIL_REMETENTE"]
+        senha_remetente = st.secrets["SENHA_REMETENTE"]
+        email_destino = st.secrets["EMAIL_DESTINO"]
 
         msg = MIMEMultipart()
         msg['From'] = email_remetente
